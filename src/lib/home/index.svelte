68197bea9fcc8fcc3204d2159dcd7ab4c2e49360
shepherdwind <eward.song@gmail.com>
2024-03-08 04:46:39 +0800

    feat: support show child every day score records

--- src/lib/home/index.svelte
+++ src/lib/home/index.svelte
@@ -15,13 +15,17 @@
    */
   export let duration: number;
 
-  console.log('cost', duration);
-  let currentUser = users?.[0]?.name || '';
+  console.log("cost", duration);
+  let currentUser = users?.[0]?.name || "";
 
   const getUserScore = (userId: number) => {
     const data = rewards.filter((reward) => reward.user_id === userId);
-    const gain = data.filter((reward) => reward.score > 0).reduce((acc, reward) => acc + reward.score, 0);
-    const cost = data.filter((reward) => reward.score < 0).reduce((acc, reward) => acc - reward.score, 0);
+    const gain = data
+      .filter((reward) => reward.score > 0)
+      .reduce((acc, reward) => acc + reward.score, 0);
+    const cost = data
+      .filter((reward) => reward.score < 0)
+      .reduce((acc, reward) => acc - reward.score, 0);
     return [gain, cost];
   };
 
@@ -34,19 +38,25 @@
     };
   });
 
-  console.log(rewards, users, scores);
+  // console.log(rewards, users, scores);
 </script>
 
 <div class="w-full max-w-xl p-4">
-  <Tabs.Root value={currentUser} onValueChange={(value) => currentUser = value || ''}>
+  <Tabs.Root
+    value={currentUser}
+    onValueChange={(value) => (currentUser = value || "")}
+  >
     <Tabs.List class="w-full">
       {#each users as user, index (user.id)}
-        <Tabs.Trigger value={user.name} class={currentUser === user.name ? 'h-24 w-2/3' :  'w-1/3 h-24'}>
+        <Tabs.Trigger
+          value={user.name}
+          class={currentUser === user.name ? "h-24 w-2/3" : "w-1/3 h-24"}
+        >
           {#if user.name === currentUser}
-          <Avatar class={currentUser === user.name ? '' : ''}>
-            <AvatarImage alt={user.name} src={user.image} />
-            <AvatarFallback>{user.name}</AvatarFallback>
-          </Avatar>
+            <Avatar class={currentUser === user.name ? "" : ""}>
+              <AvatarImage alt={user.name} src={user.image} />
+              <AvatarFallback>{user.name}</AvatarFallback>
+            </Avatar>
           {:else}
             <div class="h-24 w-1" />
           {/if}
@@ -54,16 +64,25 @@
             <h2 class="text-lg font-semibold">{user.name}</h2>
             <div class="flex items-center">
               <StarIcon className="text-yellow-400 w-5 h-5" />
-              <span class="text-yellow-600 font-semibold mx-1">{scores[index]?.gain || 0}</span>
+              <span class="text-yellow-600 font-semibold mx-1">
+                {scores[index]?.gain || 0}
+              </span>
               <StarIcon className="text-gray-400 w-5 h-5" />
-              <span class="text-gray-600 font-semibold">{scores[index]?.cost || 0}</span>
+              <span class="text-gray-600 font-semibold">
+                {scores[index]?.cost || 0}
+              </span>
             </div>
           </div>
         </Tabs.Trigger>
       {/each}
     </Tabs.List>
-		{#each users as user (user.id)}
-			<Tabs.Content value={user.name}><UserPanel id={user.id} /></Tabs.Content>
-		{/each}
+    {#each users as user (user.id)}
+      <Tabs.Content value={user.name}>
+        <UserPanel
+          rewards={rewards.filter((o) => o.user_id === user.id)}
+          id={user.id}
+        />
+      </Tabs.Content>
+    {/each}
   </Tabs.Root>
 </div>
--- src/lib/home/user.svelte
+++ src/lib/home/user.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
+  import type { Reward } from "$database/type";
   import { Calendar } from "$lib/components/ui/calendar";
   import Edit from "./edit.svelte";
   export let id: number;
-  let open = false;
+  import { today, getLocalTimeZone } from "@internationalized/date";
+  import * as Table from "$lib/components/ui/table";
+ 
+  let value = today(getLocalTimeZone());
+  export let rewards: Reward[];
+  $: currentRewards = rewards.filter((reward) => {
+    const date = reward.date;
+    // show rewards for the within 2 days of the selected date
+    return (
+      value.day === date.getDate()
+      || value.day - date.getDate() === 1)
+      && value.month === date.getMonth() + 1 && value.year === date.getFullYear();
+  }) || [];
+  const formatDate = (date: Date) => {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  };
 </script>
 
 <div class="w-full">
@@ -10,5 +26,24 @@
     <Edit id={id} />
     <Edit id={id} isAdd={false} />
   </div>
-  <Calendar class="rounded-md border shadow w-full" />
+  <Calendar bind:value={value} class="rounded-md border shadow w-full" />
+  <Table.Root class="mt-2">
+    <Table.Caption>Score within 2 days</Table.Caption>
+    <Table.Header>
+      <Table.Row>
+        <Table.Head>Reason</Table.Head>
+        <Table.Head>Score</Table.Head>
+        <Table.Head>Date</Table.Head>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {#each currentRewards as reward (reward.id)}
+        <Table.Row>
+          <Table.Cell class="font-medium">{reward.reason}</Table.Cell>
+          <Table.Cell>{reward.score}</Table.Cell>
+          <Table.Cell>{formatDate(reward.date)}</Table.Cell>
+        </Table.Row>
+      {/each}
+    </Table.Body>
+  </Table.Root>
 </div>
