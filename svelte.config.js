21ce22a01765a3b12a4dd630cf28af3a50d7429a
shepherdwind <eward.song@gmail.com>
2024-04-07 23:41:22 +0800

    refactor: Refactor service worker configuration in svelte config
    - Refactor serviceWorker configuration for better clarity and maintainability
    - Remove unused `generateSW` constant and related code

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-export const generateSW = process.env.GENERATE_SW === 'true'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,12 +8,6 @@ const config = {
       $database: './src/database',
     },
     adapter: adapter(),
-    serviceWorker: {
-      register: false,
-    },
-    files: {
-      serviceWorker: generateSW ? undefined : 'src/sw.ts',
-    },
   },
   preprocess: vitePreprocess(),
 }
